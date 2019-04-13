e4706676810e4c93de5974a222ea404309a7395e
macbook <macbook@macbooks-MacBook-Pro.local>
2019-04-14 01:17:48 +0300

    first change

---
+++ src/conteiners/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FilterForCafe from '../components/filterForCafe';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        App
+        <FilterForCafe />
+      </div>
+    );
+  }
+}
+
+export default App;
