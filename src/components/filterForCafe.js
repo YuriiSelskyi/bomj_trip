e477063c5238b95927403239359036e3a06b905c
macbook <macbook@macbooks-MacBook-Pro.local>
2019-04-14 14:21:28 +0300

    Added styles file

--- src/components/filterForCafe.js
+++ src/components/filterForCafe.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
-import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import '../styles/filter-for-cafe.css';
 
 export default class FilterForCafe extends Component {
   render () {
     return (
       <div>
         <div>
-          <Button variant="primary" size="lg" block>
-            Block level button
-          </Button>
-          <Button variant="secondary" size="lg" block>
-            Block level button
-          </Button>
-        </div>;
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic" className="dropdown">
+              TOP 10
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">TOP 5</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">TOP 20</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">TOP 100</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       </div>
     );
   }
