eacc75d3d055c2cde3a67e39401262cc3f6a7d9e
Yurii <mytsyura@gmail.com>
2019-05-04 11:34:07 +0300

    nepotrib

--- client/src/components/CafeDetails.js
+++ client/src/components/CafeDetails.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import Advertising from './Advertising';
 import MapContainer from './GoogleMap';
 import Footer from './Footer';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import Fab from '@material-ui/core/Fab';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -17,6 +19,7 @@ import {
   FaMusic,
   FaCoffee,
   FaGlassMartini,
+  FaHome,
   FaTree,
   FaClock,
   FaStar,
@@ -26,6 +29,7 @@ import {
 import '../styles/cafe-details.css';
 import '../styles/footer.css';
 
+      red: false,
 class CafeDetails extends Component {
   constructor(props) {
     super(props);
@@ -37,20 +41,33 @@ class CafeDetails extends Component {
     if (location.data !== undefined) {
       localStorage.setItem('data', JSON.stringify(location.data));
     }
-  }
+    const { data, red } = this.state;
 
+    if(red) {
+      return <Redirect to={{ pathname: '/'}} />
+    }
   renderStars = (index) => {
     return [...Array(index)].map((e, i) => <FaStar key={i}/>);
   }
 
   render () {
     const { data, red } = this.state;
+        <div className="blua">
+          <Fab
+            color="primary"
+            aria-label="Add"
+            onClick={() => this.setState({red:true})}
+            className="button-redirect-home"
+          >
+            <FaHome size={30} />
+          </Fab>
+        </div>
     const element = data.list.find((item) => item.id === data.id);
     if(red) {
       return <Redirect to={{ pathname: '/'}} />
     }
     console.log(element)
-    return (
+                image={element.photos}
       <div>
         <div className="advertising">
           <Advertising />
@@ -73,9 +90,9 @@ class CafeDetails extends Component {
                 image={element.photos}
                 title="Contemplative Reptile"
                 className="image"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2" className="title">
+
+                  <div className="icons"> {element.about}</div>
+                
                 {element.title}
                 </Typography>
                 <Typography component="div">
@@ -87,10 +104,16 @@ class CafeDetails extends Component {
                     <div >
                     <FaMapMarker />
                     {element.address}
+
+                <div>
+                </div>
                     </div>
                     <div>
                       <FaClock />
                       {element.workingHours}
+        <div>
+          <Footer/>
+        </div>
                     </div>
                   </div>
 
